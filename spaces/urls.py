cfd60186374c7a6b6d504d7faec0fb90f4f0c71c
Kiran Johns <kiranjohns369@gmail.com>
2023-08-09 21:35:37 +0530

    template base setup

--- spaces/urls.py
+++ spaces/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("destination/<str:location>", views.get_spaces, name="get_spaces"),
 ]
--- spaces/views.py
+++ spaces/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-# Create your views here.
+# Show poplular destinations and when user clicks on the popular Locations call get_spaces
+# Will have a search feature when user can enter destination name
+
 def index(request):
-    return HttpResponse("Hello from space")
+    return render(request, "index.html")
+
+# Accepts a location name and if the location name matches DB then show spaces present in location
+# User can filter the type of spaces as per all the boolean fields and add a new space
+# If location returns zero spaces, ask the user to add as primary action
+
+def get_spaces(request, location):
+    location = location.lower()
+    return render(request, "spaces.html", {"location":location})
