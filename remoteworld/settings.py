1fcfee5794c354b067eeb346dc6da26716b4453d
Kiran Johns <kiranjohns369@gmail.com>
2023-08-09 20:34:04 +0530

    basic models

--- remoteworld/settings.py
+++ remoteworld/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'spaces.apps.SpacesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'spaces',
 ]
 
 MIDDLEWARE = [
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
--- spaces/models.py
+++ spaces/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+
+class Location(models.Model):
+    location_name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.location_name
+    
+    def __repr__(self):
+        return self.location_name
+
+class Space(models.Model):
+
+    SPACE_TYPE = [
+        ("cafe", "cafe"),
+        ("coworking space", "coworking space"),
+        ("open area", "open area"),
+        ("hostel", "hostel"),
+    ]
+
+    space_name = models.CharField(max_length=200)
+    is_type = models.CharField(max_length=25,choices=SPACE_TYPE)
+    has_internet = models.BooleanField(default=False)
+    is_meeting_friendly = models.BooleanField(default=False)
+    location = models.ManyToManyField(Location)
+    url = models.URLField(max_length=200, null=True, blank=True)
+    is_approved = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.space_name
+    
+    def __repr__(self):
+        return self.space_name
+    
