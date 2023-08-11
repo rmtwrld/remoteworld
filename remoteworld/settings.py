1ec4094d0e97c8f9eea6f00c754ee77a26ee7726
Kiran Johns <kiranjohns369@gmail.com>
2023-08-11 21:18:53 +0530

    tailwind

--- remoteworld/settings.py
+++ remoteworld/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'compressor'
 ]
 
 MIDDLEWARE = [
@@ -126,6 +127,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+COMPRESS_ROOT = BASE_DIR / 'spaces/static'
+
+COMPRESS_ENABLED = True
+
+STATICFILES_FINDERS = ('compressor.finders.CompressorFinder',)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
